72523553998c78f2c230e232344c5fabbd914805
leezgithub <emailpadmaleela2017@gmail.com>
2022-07-25 18:34:28 +0530

    features202 changes committed

--- applications.java
+++ applications.java
@@ -9,3 +9,14 @@ class Main {
     System.out.println("Username is: " + userName);  // Output user input
   }
 }
+#Feature202 changes done by Simi 25-July-2022
+/ Create a class constructor for the Main class
+  public Main() {
+    x = 5;  // Set the initial value for the class attribute x
+  }
+
+  public static void main(String[] args) {
+    Main myObj = new Main(); // Create an object of class Main (This will call the constructor)
+    System.out.println(myObj.x); // Print the value of x
+  }
+}
